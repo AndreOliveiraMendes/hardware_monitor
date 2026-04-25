3e70a1136be3f8b8cf01e820949bb376957ab87d
AndreOliveiraMendes <ao_mendes@hotmail.com>
2026-04-25 18:17:39 -0300

    feat: add dynamic input types and enhance API menu with status and response handling

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -15,3 +15,25 @@ def templater_helpers(app:Flask):
     @app.template_filter('datahora')
     def format_datahora(value):
         return datetime.fromisoformat(value).strftime("%d/%m/%Y %H:%M:%S")
+    
+    @app.template_filter('input_type')
+    def input_type(it):
+        if it in ["integer", "number", "float"]:
+            return "number"
+        elif it == "boolean":
+            return "checkbox"
+        elif it in ["password", "email", "date", "time"]:
+            return it
+        elif it == "datetime":
+            return "datetime-local"
+        elif it in ["string", "text"]:
+            return "text"
+        elif it in ["url", "uri"]:
+            return "url"
+        elif it == "file":
+            return "file"
+        elif it in ["json", "object", "array", "uuid"]:
+            return "text"
+        elif it == "enum":
+            return "select"
+        return "text"
