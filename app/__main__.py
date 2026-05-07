6370eb7bc8b26505339794877ccd3fa5995d95cf
AndreOliveiraMendes <ao_mendes@hotmail.com>
2026-05-07 17:26:58 -0300

    refactor: streamline app context handling for command-line arguments

--- app/__main__.py
+++ app/__main__.py
@@ -6,11 +6,12 @@ from app.jobs.notifications import process_notifications
 from config import DEBUG_MODE, FLASK_HOST, FLASK_PORT
 
 if __name__ == "__main__":
+    app = create_app()
     if len(sys.argv) > 1:
-        if sys.argv[1] == "cleanup":
-            cleanup()
-        elif sys.argv[1] == "notifications":
-            process_notifications()
+        with app.app_context():
+            if sys.argv[1] == "cleanup":
+                cleanup()
+            elif sys.argv[1] == "notifications":
+                process_notifications()
     else:
-        app = create_app()
         app.run(host=FLASK_HOST, port=FLASK_PORT, debug=DEBUG_MODE)
